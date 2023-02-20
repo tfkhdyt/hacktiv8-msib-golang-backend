0c0398b526a92f59c8685e95b4cdd6d2c494f453
tfkhdyt <tfkhdyt@proton.me>
2023-02-20 12:19:13 +0700

    feat(sesi-1): learn about data type

--- sesi-1/project-pertama/main.go
+++ sesi-1/project-pertama/main.go
@@ -4,18 +4,35 @@ import "fmt"
 
 func main() {
 	// ini adalah komentar
-	// fmt.Println("Hello world")
+	fmt.Println("Hello world")
 	// fmt.Println("Hello", "world!", "how", "are", "you")
 	// fmt.Print("Hello", "world!", "how", "are", "you")
 
-	var firstName string = "Taufik"
-	lastName := "Hidayat"
+	// var firstName string = "Taufik"
+	// lastName := "Hidayat"
+	//
+	// fmt.Printf("halo %s %s\n", firstName, lastName)
+	//
+	// first, second, third := "satu", "dua", "tiga"
+	// fmt.Printf("%s %s %s\n", first, second, third)
+	//
+	// name, _ := "john", "wick"
+	// fmt.Println(name)
 
-	fmt.Printf("halo %s %s\n", firstName, lastName)
+	var positiveNumber uint8 = 89
+	negativeNumber := -1243423644
+	fmt.Printf("bilangan positif: %d\n", positiveNumber)
+	fmt.Printf("bilangan negatif: %d\n", negativeNumber)
 
-	first, second, third := "satu", "dua", "tiga"
-	fmt.Printf("%s %s %s\n", first, second, third)
+	decimalNumber := 2.62
+	fmt.Printf("bilangan desimal: %f\n", decimalNumber)
+	fmt.Printf("bilangan desimal: %.3f\n", decimalNumber)
 
-	name, _ := "john", "wick"
-	fmt.Println(name)
+	bool := true
+	fmt.Printf("exist? %t\n", bool)
+
+	message := `Nama saya "John Wick".
+Salam kenal.
+Mari belajar "Golang".`
+	fmt.Println(message)
 }
