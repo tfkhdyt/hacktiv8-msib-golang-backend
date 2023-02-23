6d734a913ce7e87da396ead348f2f3a10a53005a
tfkhdyt <tfkhdyt@proton.me>
2023-02-23 17:38:39 +0700

    feat(sesi-2): learn about array, slice, and map

--- sesi-2/main.go
+++ sesi-2/main.go
@@ -64,51 +64,181 @@ func main() {
 	// 	}
 	// }
 
-	for i := 0; i < 5; i++ {
-		fmt.Println("Angka", i)
+	//	for i := 0; i < 5; i++ {
+	//		fmt.Println("Angka", i)
+	//	}
+	//
+	//	i := 0
+	//	for i < 5 {
+	//		fmt.Println("Angka", i)
+	//		i++
+	//	}
+	//
+	//	j := 0
+	//	for {
+	//		fmt.Println("Angka", j)
+	//		j++
+	//		if j == 5 {
+	//			break
+	//		}
+	//	}
+	//
+	//	for k := 1; k <= 10; k++ {
+	//		if k%2 == 1 {
+	//			continue
+	//		}
+	//
+	//		if k > 8 {
+	//			break
+	//		}
+	//
+	//		fmt.Println("Angka", k)
+	//	}
+	//
+	//	for i := 0; i < 5; i++ {
+	//		for j := i; j < 5; j++ {
+	//			fmt.Print(j, " ")
+	//		}
+	//		fmt.Println()
+	//	}
+	//
+	// outerloop:
+	//
+	//	for i := 0; i < 5; i++ {
+	//		for j := 0; j < 5; j++ {
+	//			if i == 3 {
+	//				break outerloop
+	//			}
+	//			fmt.Print("matriks [", i, "][", j, "]", "\n")
+	//		}
+	//	}
+
+	// var names [4]string
+	// names[0] = "trafalgar"
+	// names[1] = "d"
+	// names[2] = "water"
+	// names[3] = "law"
+	//
+	// fmt.Println(names)
+	//
+	// fruits := [4]string{"apple", "grape", "banana", "melon"}
+	// fmt.Println("Jumlah element \t\t", len(fruits))
+	// fmt.Println("Isi semua element \t", fruits)
+	//
+	// numbers := [...]int{2, 3, 2, 4, 3}
+	// fmt.Println("data array \t:", numbers)
+	// fmt.Println("jumlah elemen \t:", len(numbers))
+	//
+	// numbers1 := [2][3]int{
+	// 	{3, 2, 3},
+	// 	{3, 4, 5},
+	// }
+	// fmt.Println("numbers1:", numbers1)
+	//
+	// for _, fruit := range fruits {
+	// 	fmt.Printf("nama buah: %s\n", fruit)
+	// }
+
+	// fruits := make([]string, 2)
+	// fruits[0] = "apple"
+	// fruits[1] = "mango"
+	// fmt.Println(fruits)
+
+	// fruits := []string{"apple", "grape", "banana", "melon"}
+	// fmt.Println(len(fruits))
+	// fmt.Println(cap(fruits))
+	//
+	// aFruits := fruits[0:3]
+	// fmt.Println(len(aFruits))
+	// fmt.Println(cap(aFruits))
+	//
+	// bFruits := fruits[1:4]
+	// fmt.Println(len(bFruits))
+	// fmt.Println(cap(bFruits))
+
+	// aaFruits := aFruits[1:2]
+	// baFruits := bFruits[0:1]
+	//
+	// fmt.Println(fruits)
+	// fmt.Println(aFruits)
+	// fmt.Println(bFruits)
+	// fmt.Println(aaFruits)
+	// fmt.Println(baFruits)
+	//
+	// baFruits[0] = "pineapple"
+	//
+	// fmt.Println(fruits)
+	// fmt.Println(aFruits)
+	// fmt.Println(bFruits)
+	// fmt.Println(aaFruits)
+	// fmt.Println(baFruits)
+
+	// cFruits := append(fruits, "papaya")
+	// fmt.Println(fruits)
+	// fmt.Println(cFruits)
+
+	// fruits := []string{"apple", "grape", "banana"}
+	// bFruits := fruits[0:2]
+	// fmt.Println(cap(bFruits))
+	// fmt.Println(len(bFruits))
+	//
+	// fmt.Println(fruits)
+	// fmt.Println(bFruits)
+	//
+	// cFruits := append(bFruits, "papaya")
+	//
+	// fmt.Println(fruits)
+	// fmt.Println(bFruits)
+	// fmt.Println(cFruits)
+
+	// dst := make([]string, 3)
+	// src := []string{"watermelon", "pineapple", "apple", "orange"}
+	// n := copy(dst, src)
+	//
+	// fmt.Println(dst)
+	// fmt.Println(src)
+	// fmt.Println(n)
+
+	// var chicken map[string]int
+	// chicken = map[string]int{}
+	//
+	// chicken["januari"] = 50
+	// chicken["februari"] = 40
+	//
+	// fmt.Println("januari", chicken["januari"])
+	// fmt.Println("mei", chicken["mei"])
+
+	// chicken := map[string]int{
+	// 	"januari":  50,
+	// 	"februari": 40,
+	// 	"maret":    34,
+	// 	"april":    67,
+	// }
+	//
+	// for key, val := range chicken {
+	// 	fmt.Println(key, " \t", val)
+	// }
+	//
+	// delete(chicken, "januari")
+	//
+	// fmt.Println(len(chicken))
+	// fmt.Println(chicken)
+	//
+	// value, isExists := chicken["mei"]
+	//
+	// if isExists {
+	// 	fmt.Println(value)
+	// } else {
+	// 	fmt.Println("item is not exists")
+	// }
+
+	chickens := []map[string]string{
+		{"name": "chicken blue", "gender": "male"},
+		{"name": "chicken red", "gender": "male"},
+		{"name": "chicken yellow", "gender": "female"},
 	}
 
-	i := 0
-	for i < 5 {
-		fmt.Println("Angka", i)
-		i++
-	}
-
-	j := 0
-	for {
-		fmt.Println("Angka", j)
-		j++
-		if j == 5 {
-			break
-		}
-	}
-
-	for k := 1; k <= 10; k++ {
-		if k%2 == 1 {
-			continue
-		}
-
-		if k > 8 {
-			break
-		}
-
-		fmt.Println("Angka", k)
-	}
-
-	for i := 0; i < 5; i++ {
-		for j := i; j < 5; j++ {
-			fmt.Print(j, " ")
-		}
-		fmt.Println()
-	}
-
-outerloop:
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
-			if i == 3 {
-				break outerloop
-			}
-			fmt.Print("matriks [", i, "][", j, "]", "\n")
-		}
+	for _, chicken := range chickens {
+		fmt.Println(chicken["gender"], chicken["name"])
 	}
 }
