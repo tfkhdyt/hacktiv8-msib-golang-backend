41441267b1c42f6a7b680a762ec0c14d1a1868e5
tfkhdyt <tfkhdyt@proton.me>
2023-03-02 07:56:27 +0700

    feat(a1/lib): create function to find student by name

---
+++ assignments/assignment-1/lib/find-student-by-name.go
@@ -0,0 +1,18 @@
+package lib
+
+import (
+	"assignment-1/data"
+	"assignment-1/domain"
+	"errors"
+	"strings"
+)
+
+func FindStudentByName(name string) (domain.Student, error) {
+	for _, student := range data.Students {
+		if strings.ToLower(student.Nama) == strings.ToLower(name) {
+			return student, nil
+		}
+	}
+
+	return domain.Student{}, errors.New("Siswa tidak ditemukan")
+}
