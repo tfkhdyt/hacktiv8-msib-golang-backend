738cbe219d6ccd77b5741f9b716307e79191266b
tfkhdyt <tfkhdyt@proton.me>
2023-03-02 08:09:32 +0700

    feat(a1/lib): add comment above functions to explain what that function does

--- assignments/assignment-1/lib/parse-name-from-args.go
+++ assignments/assignment-1/lib/parse-name-from-args.go
@@ -2,6 +2,7 @@ package lib
 
 import "errors"
 
+// function untuk mendapatkan nama siswa yang ingin dicari dari argumen-argumen
 func ParseNameFromArgs(args []string) (string, error) {
 	if len(args) < 2 {
 		return "", errors.New("Argumen tidak valid, masukkan minimal 1 nama")
