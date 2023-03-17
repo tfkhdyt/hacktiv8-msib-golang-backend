0472f916c034a96e87c320004d3216e6ff3c7fd3
tfkhdyt <tfkhdyt@proton.me>
2023-03-17 14:43:37 +0700

    feat(dto): add GetAllOrdersResponse dto

--- assignments/assignment-2/dto/order.go
+++ assignments/assignment-2/dto/order.go
@@ -3,13 +3,21 @@ package dto
 import "time"
 
 type NewOrderRequest struct {
-	OrderedAt    time.Time        `json:"orderedAt"`
-	CustomerName string           `json:"customerName"`
-	Items        []NewItemRequest `json:"items,omitempty"`
+	OrderedAt    time.Time        `json:"orderedAt"       binding:"required"`
+	CustomerName string           `json:"customerName"    binding:"required"`
+	Items        []NewItemRequest `json:"items,omitempty" binding:"required"`
 }
 
 type NewOrderResponse struct {
 	StatusCode int             `json:"statusCode"`
 	Message    string          `json:"message"`
 	Data       NewOrderRequest `json:"data"`
+}
+
+type GetAllOrdersResponse struct {
+	Id           uint                  `json:"id"`
+	CreatedAt    time.Time             `json:"createdAt"`
+	UpdatedAt    time.Time             `json:"updatedAt"`
+	CustomerName string                `json:"customerName"`
+	Items        []GetAllItemsResponse `json:"items"`
 }
