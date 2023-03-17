be89e9d5fcc63ceb4831c25f1626072d651758b7
tfkhdyt <tfkhdyt@proton.me>
2023-03-17 14:43:23 +0700

    feat(dto): add GetAllItemsResponse dto

--- assignments/assignment-2/dto/item.go
+++ assignments/assignment-2/dto/item.go
@@ -1,7 +1,19 @@
 package dto
 
+import "time"
+
 type NewItemRequest struct {
-	ItemCode    string `json:"itemCode"`
-	Description string `json:"description"`
-	Quantity    uint   `json:"quantity"`
+	ItemCode    string `json:"itemCode"    binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Quantity    uint   `json:"quantity"    binding:"required"`
+}
+
+type GetAllItemsResponse struct {
+	Id          uint      `json:"id"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	ItemCode    string    `json:"itemCode"`
+	Description string    `json:"description"`
+	Quantity    uint      `json:"quantity"`
+	OrderId     uint      `json:"orderId"`
 }
