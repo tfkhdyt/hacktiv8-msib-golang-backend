a9b48217f9e4077e9dba2a4b0bbcf0958bf83a38
tfkhdyt <tfkhdyt@proton.me>
2023-03-17 09:25:48 +0700

    feat(assignment2): create order handler

---
+++ assignments/assignment-2/handler/http_handler/order_handler.go
@@ -0,0 +1,35 @@
+package http_handler
+
+import (
+	"assignment_2/dto"
+	"assignment_2/pkg/errs"
+	"assignment_2/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type orderHandler struct {
+	orderService service.OrderService
+}
+
+func NewOrderHandler(orderService service.OrderService) *orderHandler {
+	return &orderHandler{orderService: orderService}
+}
+
+func (o *orderHandler) CreateOrder(ctx *gin.Context) {
+	var requestBody dto.NewOrderRequest
+
+	if err := ctx.ShouldBindJSON(&requestBody); err != nil {
+		newError := errs.NewUnprocessableEntity(err.Error())
+		ctx.JSON(newError.StatusCode(), newError)
+		return
+	}
+
+	newOrder, err := o.orderService.CreateOrder(requestBody)
+	if err != nil {
+		ctx.JSON(err.StatusCode(), err)
+		return
+	}
+
+	ctx.JSON(newOrder.StatusCode, newOrder)
+}
