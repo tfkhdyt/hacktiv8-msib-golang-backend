c87efacc7bf4e980ea61b75e5359a109f3c2e872
tfkhdyt <tfkhdyt@proton.me>
2023-03-17 09:26:27 +0700

    feat(handler): assign routers

--- assignments/assignment-2/handler/app.go
+++ assignments/assignment-2/handler/app.go
@@ -2,11 +2,25 @@ package handler
 
 import (
 	"assignment_2/database"
+	"assignment_2/handler/http_handler"
 	"assignment_2/repository/order_repository/order_pg"
+	"assignment_2/service"
+	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func StartApp() {
 	db := database.GetPostgresInstance()
 
 	orderRepo := order_pg.NewOrderPG(db)
+	orderService := service.NewOrderService(orderRepo)
+	orderHandler := http_handler.NewOrderHandler(orderService)
+
+	r := gin.Default()
+	r.POST("/orders", orderHandler.CreateOrder)
+
+	if err := r.Run(":3000"); err != nil {
+		log.Fatalln(err.Error())
+	}
 }
