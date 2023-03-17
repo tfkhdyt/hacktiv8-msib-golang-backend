d161c433d8a16e504bfbe75b8ad607e7f1ca30d7
tfkhdyt <tfkhdyt@proton.me>
2023-03-17 07:56:46 +0700

    feat(startapp): add order repo instance

--- assignments/assignment-2/handler/app.go
+++ assignments/assignment-2/handler/app.go
@@ -1,7 +1,12 @@
 package handler
 
-import "assignment_2/database"
+import (
+	"assignment_2/database"
+	"assignment_2/repository/order_repository/order_pg"
+)
 
 func StartApp() {
 	db := database.GetPostgresInstance()
+
+	orderRepo := order_pg.NewOrderPG(db)
 }
