b5f8ec62f8b3af065532c879e9748e18db26621f
tfkhdyt <tfkhdyt@proton.me>
2023-03-19 13:15:19 +0700

    docs(app): setup gin-swagger

--- assignments/assignment-2/handler/app.go
+++ assignments/assignment-2/handler/app.go
@@ -2,15 +2,25 @@ package handler
 
 import (
 	"assignment_2/database"
+	"assignment_2/docs"
 	"assignment_2/handler/http_handler"
 	"assignment_2/repository/order_repository/order_pg"
 	"assignment_2/service"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func StartApp() {
+	docs.SwaggerInfo.Title = "Order and Item API"
+	docs.SwaggerInfo.Description = "This is an assignment for Hacktiv8 Golang Course"
+	docs.SwaggerInfo.Version = "0.3"
+	docs.SwaggerInfo.Host = "localhost:3000"
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
 	db := database.GetPostgresInstance()
 
 	orderRepo := order_pg.NewOrderPG(db)
@@ -18,11 +28,19 @@ func StartApp() {
 	orderHandler := http_handler.NewOrderHandler(orderService)
 
 	r := gin.Default()
+
+	// Create
 	r.POST("/orders", orderHandler.CreateOrder)
+	// Get All
 	r.GET("/orders", orderHandler.GetAllOrders)
+	// Get One
 	r.GET("/orders/:orderID", orderHandler.GetOrderByID)
+	// Update
 	r.PATCH("/orders/:orderID", orderHandler.UpdateOrderByID)
+	// Delete
 	r.DELETE("/orders/:orderID", orderHandler.DeleteOrderByID)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	if err := r.Run(":3000"); err != nil {
 		log.Fatalln(err.Error())
