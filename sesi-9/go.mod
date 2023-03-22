fc00eabb3fd54515aaf60f087c0b380c56944406
tfkhdyt <tfkhdyt@proton.me>
2023-03-22 09:50:42 +0700

    feat(sesi-9): learn about vendoring

--- sesi-9/go.mod
+++ sesi-9/go.mod
@@ -1,3 +1,5 @@
 module sesi_9
 
 go 1.20
+
+require github.com/novalagung/gubrak/v2 v2.0.1
--- sesi-9/main.go
+++ sesi-9/main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	// httpRequest()
-	httpAuthBasic()
+	// httpAuthBasic()
+	goVendoring()
 }
